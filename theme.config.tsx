e45a4629c4f2203a073471344c35237027c98c6c
Joggyjagz7 <ejiro10000@gmail.com>
2025-03-25 09:21:21 +0100

    added new pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { useConfig, DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>meCash Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/meCash-docs',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+// Fix navigation configuration
+navigation: {
+  prev: true,
+  next: true
+  },
+  docsRepositoryBase: 'https://github.com/your-repo/meCash-docs/blob/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '© 2025 meCash. All rights reserved.',
   },
-}
+  primaryHue: 200, // Adjusts the main theme color
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – meCash Docs',
+      description: 'Official documentation for meCash.',
+    };
+  },
 
-export default config
+};
+
+export default config;
